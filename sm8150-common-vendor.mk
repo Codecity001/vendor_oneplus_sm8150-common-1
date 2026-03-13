f7d6638e6d4026befd7ca6c8698c833495250ac3
Quince <quinceroms@gmail.com>
2026-03-13 18:51:55 +0100

    sm8150-common: Add CVP and OmxVpp camera blobs
    Add CVP (Computer Vision Processor) and OmxVpp libraries from
    OOS 12.1, not present in TheMuppets vendor repos.
    
    These are identical across all SM8150 devices and needed by
    the OnePlus Camera for video stabilization and processing.
    
    Signed-off-by: Quince <quinceroms@gmail.com>

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -324,6 +324,7 @@ PRODUCT_PACKAGES += \
     libvpplibrary \
     libxml \
     vendor.dolby.hardware.dms@2.0 \
+    vendor.qti.hardware.cvp@1.0 \
     vendor.qti.hardware.dsp@1.0 \
     vendor.qti.hardware.vpp@1.1 \
     vendor.qti.hardware.vpp@1.2 \
@@ -479,6 +480,7 @@ PRODUCT_PACKAGES += \
     libGPTEE_vendor \
     libGPreqcancel \
     libGPreqcancel_svc \
+    libOmxVpp \
     libQSEEComAPI \
     libQTEEConnector_vendor \
     libSecureUILib \
@@ -523,7 +525,10 @@ PRODUCT_PACKAGES += \
     libcom.qti.chinodeutils \
     libcpion \
     libcvface_api \
+    libcvp \
     libcvp_common \
+    libcvp_stub \
+    libcvpcpuRev_skel \
     libdataitems \
     libdisp-aba \
     libdisplayqos \
